27041a537a92aa0c32dcfdfc4d60f5b5882bf71b
jinyoung1439 <120711382+jinyoung1439@users.noreply.github.com>
2023-06-12 20:06:12 +0900

    freeservice create
    freeservice create

--- src/main/java/com/hexas/goodgoott/free/controller/FreeController.java
+++ src/main/java/com/hexas/goodgoott/free/controller/FreeController.java
@@ -1,13 +1,14 @@
 package com.hexas.goodgoott.free.controller;
 
+import com.hexas.goodgoott.employment_info.domain.EmploymentInfo;
+import com.hexas.goodgoott.free.dto.FreeDto;
 import com.hexas.goodgoott.free.dto.FreeResponseDto;
 import com.hexas.goodgoott.free.service.FreeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +27,22 @@ public class FreeController {
         return "free/list";
     }
 
+    @GetMapping("/create")
+    public String create() {
+        return "free/create";
+    }
+
+    @PostMapping("/create/action")
+    public String write(@ModelAttribute("free") FreeDto freeDto) {
+        freeService.save(freeDto);
+        return "redirect:/free/list";
+    }
+
+    @GetMapping("/view")
+    public String getViewById(@PathVariable int freeIdx, Model model) {
+        FreeResponseDto free = freeService.getViewById(freeIdx);
+        model.addAttribute("free", free);
+        return "free/view";
+    }
 
 }
--- src/main/java/com/hexas/goodgoott/free/service/FreeService.java
+++ src/main/java/com/hexas/goodgoott/free/service/FreeService.java
@@ -1,6 +1,7 @@
 package com.hexas.goodgoott.free.service;
 
 import com.hexas.goodgoott.free.domain.FreeEntity;
+import com.hexas.goodgoott.free.dto.FreeDto;
 import com.hexas.goodgoott.free.dto.FreeResponseDto;
 import com.hexas.goodgoott.free.repository.FreeRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,17 @@ public class FreeService {
         return notification.stream()
                 .map(FreeResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public void save(FreeDto freeDto) {
+        FreeEntity free = freeDto.toEntity();
+        freeRepository.save(free);
+    }
+
+    public FreeResponseDto getViewById(int freeIdx) {
+        FreeEntity free = freeRepository.findById(freeIdx)
+                .orElseThrow(() -> new RuntimeException("free not found with ID: " + freeIdx));
+        return new FreeResponseDto(free);
+
     }
 }
